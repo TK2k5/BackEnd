860de5cc322ab0dce8dc95db31fd98ded08a1361
Triều <nhtrieu2k5@gmail.com>
2024-07-25 20:35:21 +0700

    check code, endDate, startDate vs2

--- src/routes/voucher.routes.js
+++ src/routes/voucher.routes.js
@@ -2,6 +2,7 @@ import { checkPermission } from '../middlewares/check-permission.middleware.js';
 import express from 'express';
 import { verifyToken } from '../middlewares/verify-token.middleware.js';
 import { voucherController } from '../controllers/voucher.controller.js';
+import { voucherMiddleware } from '../middlewares/voucher.middleware.js';
 import { wrapRequestHandler } from '../utils/handler.util.js';
 
 const router = express.Router();
@@ -10,6 +11,7 @@ router.post(
   '/voucher',
   wrapRequestHandler(verifyToken),
   wrapRequestHandler(checkPermission),
+  wrapRequestHandler(voucherMiddleware),
   wrapRequestHandler(voucherController.createVoucher),
 );
 
