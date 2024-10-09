45d285500bd0d7693688395c45f9b5e685ced07e
nhtrieu2k5 <nhtrieu2k5@gmail.com>
2024-10-09 22:05:18 +0700

    chat realtime

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   }),
 );
